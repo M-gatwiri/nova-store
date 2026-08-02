1a82f3d574624d10201dd5a9b9b344e72acacc63
M-gatwiri <mgatwiri806@gmail.com>
2026-08-02 14:30:40 +0300

    fixing deployment issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
-import ProductCard from "./components/ProductCard";
+// import ProductCard from "./components/ProductCard";
 import ProductList from "./components/ProductList";
 import SearchBar from "./components/SearchBar";
-import "./app.css";
+import "./App.css";
 
 function App() {
   const products = [
